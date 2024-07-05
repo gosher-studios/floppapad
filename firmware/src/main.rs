8995f18a2293212f0d59a6fcb50259a1e5ce06b8
goer <georgiytnimov@gmail.com>
2024-07-05 21:12:18 +0100

    theoretically works

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
+use core::fmt::Write;
+
 use rp2040_hal::pac::Peripherals;
 use rp2040_hal::watchdog::Watchdog;
 use rp2040_hal::sio::Sio;
 use rp2040_hal::clocks::{Clock, init_clocks_and_plls};
 use rp2040_hal::timer::Timer;
-use rp2040_hal::gpio::Pins;
+use rp2040_hal::gpio::{DynPinId, FunctionSioInput, Pin, Pins, PullUp};
 use rp2040_hal::i2c::I2C;
 use rp2040_hal::usb::UsbBus;
 use rp2040_hal::fugit::{ExtU32, RateExtU32};
@@ -24,6 +26,7 @@ use floppapad_firmware::{PRODUCT_ID, PRODUCT_NAME, VENDOR_ID, VENDOR_NAME};
 
 use defmt_rtt as _;
 use panic_probe as _;
+use usbd_human_interface_device::UsbHidError;
 
 #[used]
 #[link_section = ".boot2"]
@@ -69,44 +72,93 @@ unsafe fn main() -> ! {
   let mut usb_dev = UsbDeviceBuilder::new(&usb_alloc, UsbVidPid(VENDOR_ID, PRODUCT_ID))
     .product(PRODUCT_NAME)
     .manufacturer(VENDOR_NAME)
-    .serial_number("real")
+    .serial_number("TEST")
     .build();
 
-  // let oled_i2c = I2C::i2c0(
-  //   pac.I2C0,
-  //   pins.gpio28.into_function(),
-  //   pins.gpio29.into_function(),
-  //   400.kHz(),
-  //   &mut pac.RESETS,
-  //   clocks.system_clock.freq(),
-  // );
-  // let mut oled = Ssd1306::new(
-  //   I2CDisplayInterface::new(oled_i2c),
-  //   DisplaySize128x32,
-  //   DisplayRotation::Rotate0,
-  // )
-  // .into_terminal_mode();
-  // oled.init().unwrap();
-  // oled.print_char('f').unwrap();
+  let mut keys: [Pin<DynPinId, FunctionSioInput, PullUp>; 10] = [
+    pins.gpio16.into_pull_up_input().into_dyn_pin(), // top left
+    pins.gpio17.into_pull_up_input().into_dyn_pin(), // middle left
+    pins.gpio18.into_pull_up_input().into_dyn_pin(), // bottom left
+    pins.gpio19.into_pull_up_input().into_dyn_pin(), // top middle
+    pins.gpio20.into_pull_up_input().into_dyn_pin(), // middle middle
+    pins.gpio21.into_pull_up_input().into_dyn_pin(), // bottom middle
+    pins.gpio24.into_pull_up_input().into_dyn_pin(), // top right
+    pins.gpio25.into_pull_up_input().into_dyn_pin(), // middle right
+    pins.gpio26.into_pull_up_input().into_dyn_pin(), // bottom right
+    pins.gpio27.into_pull_up_input().into_dyn_pin(), // sigma key TODO rename
+  ];
+  let oled_i2c = I2C::i2c0(
+    pac.I2C0,
+    pins.gpio28.into_function(),
+    pins.gpio29.into_function(),
+    400.kHz(),
+    &mut pac.RESETS,
+    clocks.system_clock.freq(),
+  );
+  let mut oled = Ssd1306::new(
+    I2CDisplayInterface::new(oled_i2c),
+    DisplaySize128x32,
+    DisplayRotation::Rotate0,
+  )
+  .into_terminal_mode();
+  oled.init().unwrap();
+  oled.print_char('f').unwrap();
 
   watchdog.start(1.secs());
   let mut usb_tick = timer.count_down();
   usb_tick.start(1.millis());
-  let mut secs = timer.count_down();
-  secs.start(1.secs());
+  let mut input_tick = timer.count_down();
+  input_tick.start(10.millis());
   loop {
-    let mut keys = [Keyboard::NoEventIndicated];
-    if secs.wait().is_ok() {
-      keys = [Keyboard::A];
+    if input_tick.wait().is_ok() {
+      let keys = get_keys(&mut keys);
+      for i in 0..9 {
+        if !(keys[i] == Keyboard::NoEventIndicated) {
+          oled.write_char('e').unwrap();
+          oled.clear().unwrap();
+        }
+      }
+      match usb_hid.device().write_report(keys) {
+        Ok(_) => {}
+        Err(UsbHidError::WouldBlock) => {}
+        Err(UsbHidError::Duplicate) => {}
+        Err(e) => core::panic!("joever {:?}", e),
+      };
     }
-    usb_hid.device().write_report(keys);
     if usb_tick.wait().is_ok() {
-      usb_hid.tick();
+      match usb_hid.tick() {
+        Ok(_) => {}
+        Err(UsbHidError::WouldBlock) => {}
+        Err(e) => core::panic!("joever {:?}", e),
+      };
     }
     if usb_dev.poll(&mut [&mut usb_hid]) {
-      let _ = usb_hid.device().read_report();
-      // defmt::info!("data");
+      match usb_hid.device().read_report() {
+        Ok(_) => {}
+        Err(_) => {}
+      };
     }
     watchdog.feed();
   }
+}
+
+fn get_keys(keys: &mut [Pin<DynPinId, FunctionSioInput, PullUp>]) -> [Keyboard; 10] {
+  let mut key_codes = [
+    Keyboard::A,
+    Keyboard::B,
+    Keyboard::C,
+    Keyboard::D,
+    Keyboard::E,
+    Keyboard::F,
+    Keyboard::G,
+    Keyboard::H,
+    Keyboard::I,
+    Keyboard::J,
+  ];
+  for i in 0..9 {
+    if !keys[i].is_low().unwrap() {
+      key_codes[i] = Keyboard::NoEventIndicated
+    }
+  }
+  key_codes
 }
